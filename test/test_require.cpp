be95e1fb8411277482c1a171f9b10774b1bfd985
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2021-04-11 14:39:35 -0700

    Reword test description for `REQUIRE` tests

--- test/test_require.cpp
+++ test/test_require.cpp
@@ -2,11 +2,11 @@
 
 #include "mute/mute.h"
 
-SCENARIO( "Require prevents any further testing", "" ) {
+SCENARIO( "`REQUIRE` prevents any further testing", "" ) {
     CHECK( false );
     CHECK( false );
 
-    GIVEN( "a section after failed require" ) {
+    GIVEN( "a section after failed `REQUIRE`" ) {
         THEN( "the section is not executed nor visible" ) {
             REQUIRE( false );
             REQUIRE( false );
@@ -14,18 +14,22 @@ SCENARIO( "Require prevents any further testing", "" ) {
     }
 }
 
-SCENARIO( "Require is section allows further section", "" ) {
+SCENARIO( "`REQUIRE` statement in section allows further section to run", "" ) {
     using namespace mute;
     int v = 123;
 
-    GIVEN( "a first section before any failed require" ) {
-        THEN( "the section is executed" ) {
+    GIVEN( "a first section before any failed `REQUIRE`" ) {
+        THEN(
+            "the section is executed, and the first failed `REQUIRE` is "
+            "reported" ) {
             REQUIRE_THAT( v, lt( 123 ) );
             REQUIRE( false );
         }
     }
     GIVEN( "a second section after a section containing s failed require" ) {
-        THEN( "the section is executed" ) {
+        THEN(
+            "the section is executed, and the first failed `REQUIRE` is "
+            "reported" ) {
             REQUIRE_THAT( v, gt( 123 ) );
             REQUIRE( false );
         }
